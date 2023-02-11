2f468a1a9a858a9ada87ecf765bbdd7c95a478e1
SanaHafez <sanahafez91@gmail.com>
2023-02-11 23:34:56 +0400

    pump code

--- robotcode.py
+++ robotcode.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, render_template, redirect, url_for, make_response
 import time
+import os
 import RPi.GPIO as GPIO
 from gpiozero import LED, Motor, Button, AngularServo
 from time import sleep
-
+#from camera_pi import Camera
+#<img src="{{ url_for('video_feed') }}" width="50%">
 idle, Forward, Backward, Left, Right, camOnEntry, camOnStay, camTilt, camPan, camCenter, camOff,sensorIdle, sensorDown, sensorOnstay, sensorUP  = range(15)
 current = idle
-current1='Idle'
+current1="Idle"
 #dc motor
 motor1 = Motor(forward=23, backward=24)
 motor2 = Motor(forward=25, backward=26)
+pump=Motor(9)
 
 #cam pan tilt
 panServo =  AngularServo(27, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
@@ -85,7 +88,7 @@ def drive():
     global current
     global current1
     if current == idle:
-        current1='Idle'
+        current1='Stop'
         motor1.stop()
         motor2.stop()
     elif current == Forward:
@@ -105,68 +108,88 @@ def drive():
         motor1.backward(speed=1)
         motor2.forward(speed=1)
 #page
+mgs='idle'
+sprinkle='Off'
+sensor='Reading'
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def index():
-    return render_template('index.html')
-
-@app.route('/<msg>/<value>')
-def move(msg, value):
     global panServoAngle
     global tiltServoAngle
     global led
     global current
     global current1
-    if msg == 'pan':
-        current=idle
-        if value == 'left':
-            panServoAngle = panServoAngle + 10
-        else:
-            panServoAngle = panServoAngle - 10
+    global msg
+    global sprinkle
+    global sensor
+    if request.method == "POST":
+        msg = request.form['action']
+    #return render_template('index.html')
+
+#@app.route('/<msg>/<value>')
+#def move(msg, value):
+    
+        if msg == 'panleft':
+            current=idle
+            if panServoAngle<100:
+                panServoAngle = panServoAngle + 10
+        elif msg == 'panright':
+            if panServoAngle>50:
+                panServoAngle = panServoAngle - 10
     # os.system("python3 angleServoCtrl.py " + str(panPin) + " " + str(panServoAngle))
-    elif msg == 'tilt':
-        current = idle
-        if value == 'up':
-            tiltServoAngle = tiltServoAngle + 10
-        else:
-            tiltServoAngle = tiltServoAngle - 10
+        elif msg == 'tiltup':
+            current = idle
+            if tiltServoAngle<100:
+                tiltServoAngle = tiltServoAngle + 10
+        elif msg == 'tiltdown':
+            if tiltServoAngle>50:
+                tiltServoAngle = tiltServoAngle - 10
     # os.system("python3 angleServoCtrl.py " + str(tiltPin) + " " + str(tiltServoAngle))
-    elif msg == 'led':
-        if value == 'on':
+        elif msg == 'ledon':
             led = "On"
             ledPin.on()
             print("led On")
-        else:
+        elif msg == 'ledoff':
             led = "Off"
             ledPin.off()
             print("led Off")
-    elif msg =='drive':
-        if value == 'stop':
+        elif msg =='stop':
             current = idle
             drive()
-        elif value =='backward':
+        elif msg =='forward':
+            current = Forward
+            drive()
+        elif msg =='backward':
             current = Backward
             drive()
-        elif value =='backward':
-            current = Backward
+        elif msg =='left':
+            current = Left
             drive()
-        elif value =='left':
-           current = Left
-           drive()
-        elif value =='right':
-           current = Right
-           drive()
-    elif msg == 'testSoil':
-           current = sensorIdle
+        elif msg =='right':
+            current = Right
+            drive()
+        elif msg == 'soiltest':
+            current = sensorIdle
+            sensor='sensorValue' ##Edit here
+        elif msg == 'sprinkleon':
+            sprinkle='On'
+            pump.on()
+            current = sensorIdle
+        elif msg == 'sprinkleoff':
+            sprinkle='Off'
+            pump.off()
+            current = sensorIdle
+               
 
-    
 
     templateData = {
         'panServoAngle': panServoAngle,
         'tiltServoAngle': tiltServoAngle,
         'led': led,
-        'current' : current1
+        'current1' : current1,
+        'sprinkle' : sprinkle,
+        'sensor': sensor
     }
     # response = make_response(redirect(url_for('index')))
     # return(response)
@@ -174,4 +197,5 @@ def move(msg, value):
     return render_template('index.html', **templateData)
 
 if __name__ == '__main__':
+    os.system("sudo rm -r  ~/.cache/chromium/Default/Cache/*")
     app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
