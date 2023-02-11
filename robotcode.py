46edab3580957aede6cb9e7ac1459495fb71b70d
SanaHafez <sanahafez91@gmail.com>
2023-02-11 14:49:30 +0400

    code file update

--- robotcode.py
+++ robotcode.py
@@ -1,147 +1,141 @@
-from gpiozero import Motor,Button,AngularServo
+from flask import Flask, request, render_template, redirect, url_for, make_response
+import time
 import RPi.GPIO as GPIO
+from gpiozero import LED, Motor, Button, AngularServo
+from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-sensor = 3
-GPIO.setup(sensor, GPIO.IN)
-
-Idle, Forward, Backward, Left, Right, camOnEntry, camOnStay, camTilt, camPan. camCenter, camOff,sensorIdle, sensorDown, sensorOnstay, sensorUP  = range(15)
-current = Idle
-sensorCurrent=sensorIdle
-camCurrent == camOnEntry
+idle, Forward, Backward, Left, Right, camOnEntry, camOnStay, camTilt, camPan, camCenter, camOff,sensorIdle, sensorDown, sensorOnstay, sensorUP  = range(15)
+current = idle
 
 motor1 = Motor(forward=23, backward=24)
 motor2 = Motor(forward=25, backward=26)
-servo1 = AngularServo(26, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
-servo2 = AngularServo(27, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
-
-FPB = Button(18)
-BPB = Button(19)
-LPB = Button(20)
-RPB = Button(21)
-servoButton1 =Button(15)
-servoButton2 =Button(14)
-servoButton3 =Button(13)
-servoButton4 =Button(12)
-measureSoil =Button(11)
-
+panServo =  AngularServo(26, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+tiltServo =  AngularServo(27, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+steps = 800
 coil1 = Motor(forward=18, backward=23, pwm=False) #sensor stepper
 coil2 = Motor(forward=24, backward=17, pwm=False)
-
 forward_seq = ['FF', 'BF', 'BB', 'FB']
 reverse_seq = list(forward_seq)  # to copy the list
 reverse_seq.reverse()
-steps = 800
-
-def measureSoilButton():
-    sensorDown()
-    Timer(5, timerSensor).start()
-
+panServoAngle = 90
+tiltServoAngle = 90
+panServo.angle=panServoAngle
+tiltServo.angle=tiltServoAngle
+led = "Off"
+ledPin = LED(21)
 def timerSensor():
     global current
     current = sensorOnstay
 
-measureSoil.when_pressed= measureSoilButton
-def sensorGoesDown():
-    for i in range(steps):
-        for step in forward_seq:
-            set_step(step)
+app = Flask(__name__)
 
-def sensorGoesUp():
-    for i in range(steps):
-        for step in reverse_seq:
-            set_step(step)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def servo1Press():
-    servo1.angle += 5
-
-def servo2Press():
-    servo1.angle -= 5
-
-def servo3Press():
-    servo2.angle += 5
-
-def servo4Press():
-    servo2.angle -= 5
-
-servoButton1.when_pressed= servo1Press
-servoButton2.when_pressed= servo2Press
-servoButton3.when_pressed= servo3Press
-servoButton4.when_pressed= servo4Press
-
-def FPBpressed():
+@app.route('/<msg>/<value>')
+def move(msg, value):
+    global panServoAngle
+    global tiltServoAngle
+    global led
     global current
-    print("FPBpressed")
-    if current == Idle:
-        current = Forward
-
-FPB.when_pressed = FPBpressed
-
-def BPBpressed():
-    global current
-    print("BPBpressed")
-    if current == Idle:
-        current = Backward
-
-BPB.when_pressed = BPBpressed
-
-def LPBpressed():
-    global current
-    print("LPBpressed")
-    if current == Idle:
-        current = Left
-LPB.when_pressed = LPBpressed
-
-def RPBpressed():
-    global current
-    print("RPBpressed")
-    if current == Idle:
-        current = Right
-RPB.when_pressed = RPBpressed
-
+    if msg == 'pan':
+        current=idle
+        if value == 'left':
+            panServoAngle = panServoAngle + 10
+        else:
+            panServoAngle = panServoAngle - 10
+    # os.system("python3 angleServoCtrl.py " + str(panPin) + " " + str(panServoAngle))
+    elif msg == 'tilt':
+        current = idle
+        if value == 'up':
+            tiltServoAngle = tiltServoAngle + 10
+        else:
+            tiltServoAngle = tiltServoAngle - 10
+    # os.system("python3 angleServoCtrl.py " + str(tiltPin) + " " + str(tiltServoAngle))
+    elif msg == 'led':
+        if value == 'on':
+            led = "On"
+            ledPin.on()
+            print("led On")
+        else:
+            led = "Off"
+            ledPin.off()
+            print("led Off")
+    elif msg =='drive':
+        if value == 'forward'
+            current = Forward
+            drive()
+        elif value =='backward':
+            current = Backward
+            drive()
+        elif value =='left':
+           current = Left
+           drive()
+        elif value =='right':
+           current = Right
+           drive()
+    elif msg == 'testSoil':
+           current = sensorIdle
 
 def drive():
-    if current == Idle:
+    if current == idle:
         motor1.stop()
         motor2.stop()
     elif current == Forward:
         camCurrent = camOnEntry
-        motor1.forward(speed=speed)
-        motor2.forward(speed=speed)
+        motor1.forward(speed=1)
+        motor2.forward(speed=1)
     elif current == Backward:
-        motor1.backward(speed=speed)
-        motor2.backward(speed=speed)
+        motor1.backward(speed=1)
+        motor2.backward(speed=1)
     elif current == Left:
-        motor1.forward(speed=speed)
-        motor2.backward(speed=speed)
+        motor1.forward(speed=1)
+        motor2.backward(speed=1)
     elif current == Right:
-        motor1.backward(speed=speed)
-        motor2.forward(speed=speed)
+        motor1.backward(speed=1)
+        motor2.forward(speed=1)
 
-def moveCam():
-    if camCurrent == camOnEntry:
-        servo1.angle = 0
-        servo2.angle = 0
+def sensorGoesDown():
+    for i in range(steps):
+        for step in forward_seq:
+            set_step(steps)
+
+def sensorGoesUp():
+    for i in range(steps):
+        for step in reverse_seq:
+            set_step(steps)
 
 def stepper():
     global current
-    if current== sensorIdle:
+    if current == sensorIdle:
         print("Idle")
     elif current == sensorDown:
         sensorGoesDown()
-        current=sensorOnstay
+        current = sensorOnstay
     elif current == sensorOnstay:
         moisturevalue = GPIO.input(pin)
         if moisturevalue == 0:
             print("Moisture detected")
-        elif moisturevalue == 1:
+        else:
             print("Moisture NOT detected")
-        moistureCurrent=sensorUP
+            moistureCurrent = sensorUP
     elif current == sensorUP:
         sensorGoesUp()
-        current=sensorIdle
-
+        current = sensorIdle
 while True:
     drive()
-    moveCam()
     stepper()
+
+    templateData = {
+        'panServoAngle': panServoAngle,
+        'tiltServoAngle': tiltServoAngle,
+        'led': led
+    }
+    # response = make_response(redirect(url_for('index')))
+    # return(response)
+
+    return render_template('index.html', **templateData)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8080, threaded=True)
